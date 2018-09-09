3cd2840045b95b7cd62a77eec7721cf222fe9086
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-09-09 17:06:53 +0530

    icons added to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegsitry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegsitry.addSvgIcon(
+      'settings',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-settings-20px.svg')
+    );
+    iconRegsitry.addSvgIcon(
+      'add',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-add-24px.svg')
+    );
+    iconRegsitry.addSvgIcon(
+      'navigate_next',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-navigate_next-24px.svg')
+    );
+    iconRegsitry.addSvgIcon(
+      'edit',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-edit-24px.svg')
+    );
+  }
 
   ngOnInit() {
   }
