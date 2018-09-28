4bc41782fb08637fafd2dd0d08c79f1fc8bba489
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-09-28 12:28:45 +0530

    home page made dynamic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { PostsService } from '../../services/posts/posts.service';
+import { HomeViewModel } from '../../models/home.model';
+import { CategoriesService } from '../../services/categories/categories.service';
+import { UsersService } from '../../services/users/users.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +13,10 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(iconRegsitry: MatIconRegistry, sanitizer: DomSanitizer) {
+  model: HomeViewModel;
+
+  constructor(iconRegsitry: MatIconRegistry, sanitizer: DomSanitizer,
+    private postService: PostsService, private categoryService: CategoriesService, private userService: UsersService) {
     iconRegsitry.addSvgIcon(
       'settings',
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-settings-20px.svg')
@@ -19,16 +26,25 @@ export class HomeComponent implements OnInit {
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-add-24px.svg')
     );
     iconRegsitry.addSvgIcon(
-      'navigate_next',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-navigate_next-24px.svg')
+      'label',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-label-24px.svg')
     );
     iconRegsitry.addSvgIcon(
       'edit',
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-edit-24px.svg')
     );
+    iconRegsitry.addSvgIcon(
+      'folder',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-folder-24px.svg')
+    );
+    iconRegsitry.addSvgIcon(
+      'people',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-people-24px.svg')
+    );
   }
 
   ngOnInit() {
+    this.model = new HomeViewModel(this.postService.getDemoData(), this.categoryService.getDemoData().length, this.userService.getDemoData().length);
   }
 
 }
