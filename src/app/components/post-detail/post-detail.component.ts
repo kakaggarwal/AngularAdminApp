dd8e322fae41dad3a450fb70b5834fbb3422d90e
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-09-30 20:11:10 +0530

    details page html added

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostsService } from '../../services/posts/posts.service';
+import { PostViewModel } from '../../models/posts.model';
 
 @Component({
   selector: 'app-post-detail',
@@ -8,8 +9,7 @@ import { PostsService } from '../../services/posts/posts.service';
   styleUrls: ['./post-detail.component.scss']
 })
 export class PostDetailComponent implements OnInit {
-
-
+  model: PostViewModel;
 
   constructor(private activeRoute: ActivatedRoute, private postService: PostsService) { }
 
@@ -20,7 +20,7 @@ export class PostDetailComponent implements OnInit {
   getPostDetails(): void {
     const postId = +this.activeRoute.snapshot.paramMap.get('id');
 
-    this.
+    this.postService.getPostDetails(postId).subscribe(data => this.model = data);
   }
 
 }
