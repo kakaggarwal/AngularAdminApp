86416c8d40112f680deab131a71d11e2089f65af
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-10-08 11:55:01 +0530

    model classed created for category component

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,9 +1,20 @@
 
-export class CategoryViewModel {
+export class CategoryModel {
   cateogryId: number;
   title: string;
 
-  constructor(title: string) {
+  constructor(categoryId: number, title: string) {
+    this.cateogryId = categoryId;
     this.title = title;
+  }
+}
+
+export class CategoryViewModel extends CategoryModel {
+  datePosted: Date;
+
+  constructor(category: CategoryModel, datePosted: Date) {
+    super(category.cateogryId, category.title);
+
+    this.datePosted = datePosted;
   }
 }
