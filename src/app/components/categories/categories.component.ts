4a11d360cfd5612dbf04975a6f6cea6eb6dddb6a
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-10-08 11:53:24 +0530

    categories component html and code completed

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CategoriesService } from '../../services/categories/categories.service';
+import { CategoryViewModel } from '../../models/category.model';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  searchValue: string = '';
 
-  constructor() { }
+  model: CategoryViewModel[] = [];
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer,
+    private categoryService: CategoriesService
+  ) {
+    iconRegistry.addSvgIcon(
+      'folder',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-folder-24px.svg')
+    );
+  }
 
   ngOnInit() {
+    this.categoryService.getCategoriesView().subscribe(data => {
+      this.model = data;
+    });
   }
 
 }
