ecc030df895c77505f7d8efda73b611bb0ae60c3
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-10-08 11:55:54 +0530

    model classes created for home component

--- src/app/models/home.model.ts
+++ src/app/models/home.model.ts
@@ -5,11 +5,4 @@ export class HomeViewModel {
   postCount: number;
   categoryCount: number;
   userCount: number;
-
-  constructor(posts: PostViewModel[], categoryCount: number, userCount: number) {
-    this.posts = posts;
-    this.postCount = posts.length;
-    this.categoryCount = categoryCount;
-    this.userCount = userCount;
-  }
 }
