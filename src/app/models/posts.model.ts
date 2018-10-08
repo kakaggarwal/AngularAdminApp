6e3ae57c42e73a63de8524b2aeab08a8f3adbd5a
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-10-08 11:56:13 +0530

    post model classes created for post component

--- src/app/models/posts.model.ts
+++ src/app/models/posts.model.ts
@@ -1,15 +1,30 @@
 
-export class PostViewModel {
+export class PostModel {
   postId: number;
   title: string;
-  category: string;
+  category: number;
   body: string;
   datePosted: Date;
 
-  constructor(title: string, category: string, datePosted: Date) {
+  constructor(postId: number, title: string, category: number, datePosted: Date) {
+    this.postId = postId;
     this.title = title;
     this.category = category;
     this.datePosted = datePosted;
     this.body = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore, repudiandae ad exercitationem totam expedita incidunt praesentium, voluptas necessitatibus beatae possimus architecto? Culpa vel ipsa aspernatur eligendi sequi eaque recusandae.";
   }
+}
+
+export class PostViewModel extends PostModel {
+  categoryName: string;
+
+  constructor(post: PostModel, categoryName: string) {
+    super(post.postId, post.title, post.category, post.datePosted);
+
+    this.categoryName = categoryName;
+  }
+}
+
+export class PostDetailViewModel extends PostModel {
+
 }
