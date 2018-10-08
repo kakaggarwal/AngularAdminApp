d6cf9cc7db6669f9710a8d62ffc595242083befb
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-10-08 11:56:54 +0530

    user service created for user component

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
 import { UserViewModel } from '../../models/user.model';
+import { Observable, of } from 'rxjs';
+import { appDemoData } from '../../models/common.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private demoData = appDemoData.users;
 
   constructor() { }
 
-  getDemoData(): UserViewModel[] {
-    let data = [
-      new UserViewModel('John Doe', 'johndoe@gmail.com'),
-      new UserViewModel('Brad Traversy', 'bradtraversy@gmail.com'),
-      new UserViewModel('Jane Doe', 'janedoe@gmail.com'),
-      new UserViewModel('Rock Clifford', 'rockclifford@gmail.com'),
-    ];
-
-    data.forEach((value, index) => { value.userId = (index + 1) });
-
-    return data;
+  getUsers(): Observable<UserViewModel[]> {
+    return of(this.demoData);
   }
 }
