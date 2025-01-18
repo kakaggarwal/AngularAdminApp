27369191ddb08d52fee5fa6f4e4391aea01507d8
Krishan Aggarwal <kakaggarwal@gmail.com>
2025-01-18 16:46:15 +0530

    Angular version updated to v17

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { PostsService } from '../../services/posts/posts.service';
 import { HomeViewModel } from '../../models/home.model';
 import { CategoriesService } from '../../services/categories/categories.service';
